d3171c7ba373bf6200f36d1c76f0ae2255d8ef09
Amange1di <amangeldi2702006@gmail.com>
2024-11-22 20:07:46 +0600

    modified:   src/pages/contact/Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -6,8 +6,10 @@ const Contact = () => {
     <div className="contact-container">
       <h2>Контактная информация</h2>
       <address className="contact-details">
-        {/* <p>+996 706 343 568</p> */}
+        <p>+996 706 343 568</p>
+
         <p><a href="mailto:amangeldi2702006@gmail.com">amangeldi2702006@gmail.com</a></p>
+        <p><a href="https://t.me/Amaangeldi">Amaangeldi</a></p>
       </address>
     </div>
   )
